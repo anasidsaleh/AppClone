e7b13e2f13f86cb372b93592332462c89124c77c
anasidsaleh <a.idsaleh@enim.ac.ma>
2024-05-12 22:44:56 +0000

    Add the icon and some dump text

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from "./store";
+import HomeScreen from './screens/HomeScreen';
 
-export default function App() {
+// 1) set up redux
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello Nihal </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <HomeScreen/>
+    </Provider>
+
   );
 }
+
+export default App
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  
 });
